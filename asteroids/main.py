25509c6c1a124614b799a95a2b8b7fc4279915b3
natale565 <natale565@icloud.com>
2026-03-25 23:22:00 -0400

    added files and imported screen_width and screen_height

---
+++ asteroids/main.py
@@ -0,0 +1,12 @@
+import pygame
+from constants import SCREEN_WIDTH
+from constants import SCREEN_HEIGHT
+
+def main():
+    print("Starting Asteroids with pygame version: VERSION")
+    print('Screen width: 1280')
+    print('Screen height: 720')
+
+
+if __name__ == "__main__":
+    main()
