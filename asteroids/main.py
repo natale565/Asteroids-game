797b752964d0ba939023a5c8fde7cee8586cf2fd
natale565 <natale565@icloud.com>
2026-03-31 11:59:58 -0400

    grouped movement

--- asteroids/main.py
+++ asteroids/main.py
@@ -17,7 +17,16 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
+    
 
     while True:
         log_state()
@@ -25,8 +34,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
