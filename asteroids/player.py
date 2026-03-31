556e4111242af66f687c5e4cfa8a940c8fc8ee8c
natale565 <natale565@icloud.com>
2026-03-30 21:10:19 -0400

    ability to move up and down

--- asteroids/player.py
+++ asteroids/player.py
@@ -2,6 +2,7 @@ from circleshape import CircleShape
 from constants import PLAYER_RADIUS
 from constants import LINE_WIDTH
 from constants import PLAYER_TURN_SPEED
+from constants import PLAYER_SPEED
 import pygame
 
 
@@ -31,5 +32,17 @@ class Player(CircleShape):
         if keys[pygame.K_a]:
             self.rotate(-dt)
 
+        if keys[pygame.K_w]:
+            self.move(dt)
+        
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
         if keys[pygame.K_d]:
             self.rotate(dt)
+
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
